85ae6177fa95b68c1e4fa638a1dc76009702eab2
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-04-01 15:08:39 -0700

    slider

--- Planckcito.py
+++ Planckcito.py
@@ -7,16 +7,20 @@ st.write("el valor de click es:", click)
 if click==True:
     st.image("R.jpg")
     
-num1 = st.slider('Elige el numero 1', 0.0,100.0,25.0)
-num2 = st.slider('Elige el numero 2',0.0,100.0,25.0)
-suma = num1+num2
-st.write("la suma de los dos es:", suma)
+
 
 st.write("ahora multipliquemos")
 nn1 = st.number_input("dame n1")
 nn2 = st.number_input("dame n2")
 mult = nn1*nn2
 st.write("la multiplicacion da",mult)
+
+with st.sidebar:
+    num1 = st.slider('Elige el numero 1', 0.0,100.0,25.0)
+    num2 = st.slider('Elige el numero 2',0.0,100.0,25.0)
+    suma = num1+num2
+    st.write("la suma de los dos es:", suma)
+
 #Las st.button pusieron botones
 #st.button("dale click")
 #st.button("Otro boton")
