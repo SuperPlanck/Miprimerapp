17828969c5262dcdcd57e4d4af6acd71bee18df1
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-04-01 14:22:10 -0700

    libreria

--- Planckcito.py
+++ Planckcito.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 st.title("miprimerapp")
 #st.button("dale click")
 #st.button("Otro boton")
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+st.write(df)
+st.map(df)
