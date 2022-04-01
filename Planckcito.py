889987a18c93007ad941e0cea1812cf0f6d3ce33
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-04-01 15:02:06 -0700

    suma

--- Planckcito.py
+++ Planckcito.py
@@ -7,6 +7,10 @@ st.write("el valor de click es:", click)
 if click==True:
     st.image("R.jpg")
     
+num1 = st.slider('Elige el numero 1', 0.0,100.0,25.0)
+num2 = st.slider('Elige el numero 2',0.0,100.0,25.0)
+suma = num1+num2
+st.write("la suma de los dos es:", suma)
 
 #Las st.button pusieron botones
 #st.button("dale click")
